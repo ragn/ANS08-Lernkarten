fd3c36aa4525e8caa14c0ecc3e2b981958a13c41
Ralf Gnädinger <ralf.gnaedinger@gmail.com>
2014-09-10 11:16:58 +0200

    pdflatex bindet nun die Abhaengigkeiten korrekt ein.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 ANS08-Lernkarten
 ================
+
+# Kommandos:
+
+> make init
+
+Läd die Anhängigkeiten
+
+> make build
+
+Erstellt die Lernkarten
+
+> make clean
+
+Löscht nicht versionierte Dateien
+
+> make commit
+
+Fügt alle Änderungen und neue Dateien hinzu 
+
+#Abhängigkeiten
+
+* https://github.com/kellertuer/Kartei
--- make.bat
+++ make.bat
@@ -1,3 +1,36 @@
 @echo off
 
-pdflatex --include-directory=../../_latex lernkarten.tex
+
+if "%1" == "" GOTO build
+
+if "%1" == "build" GOTO build
+
+if "%1" == "init" GOTO init
+
+
+
+:init
+	git submodule init
+GOTO exit
+
+
+
+:build
+	pdflatex --include-directory=lib/kartei lernkarten.tex
+	pdflatex --include-directory=lib/kartei lernkarten.tex
+GOTO clean
+
+
+	
+:clean
+	git clean -fx
+GOTO exit
+
+
+
+:commit
+	git add .
+	git commit	
+GOTO exit
+
+:exit
